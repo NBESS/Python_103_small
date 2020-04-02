84e6137b11fe133415bbf64a41ac53fe7cce479b
NBESS <nicholasdbess@gmail.com>
2020-04-02 14:58:49 -0400

    adds comments and variables

---
+++ python_exercise10.py
@@ -0,0 +1,13 @@
+# Print a square the size of user's input
+
+# Setup 
+# Create variables to be used 
+
+shape_item = ''
+user_request = int(input('How big is the square? '))
+count = 0
+
+# Perform the task
+
+
+# Output the result
