e702bcbb6a919ac27280e72ce875d1855e1774ab
NBESS <nicholasdbess@gmail.com>
2020-04-02 13:40:21 -0400

    adds code to complete task

--- python_exercise9.py
+++ python_exercise9.py
@@ -1,9 +1,16 @@
 # Print a 5x5 square of "*"
 
 # Setup
-
+shape_item = ''
+lenght_of_shape = 5
+count = 0
 
 # Do the work
+# Have shape item print desired length
 
+while count < lenght_of_shape:
+    shape_item = shape_item + '*'
+    count += 1
+    # Provide the output
+    print(shape_item,flush=True)
 
-# Provide the output
