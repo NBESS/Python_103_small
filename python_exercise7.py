b50f0af54a50340fed5d3e055c9d00f1ea75b709
NBESS <nicholasdbess@gmail.com>
2020-04-02 11:34:01 -0400

    adds while loop

--- python_exercise7.py
+++ python_exercise7.py
@@ -1,11 +1,14 @@
 # Print out the numbers from 1 to 10 inclusively, with each number appearing on it's own line
 
 # Setup
-
+# add variable outside of loop
+count = 0
 
 
 # Do the work
+while count <= 10:
+    count += 1
+    # Output
+    print(count)
 
 
-
-# Output
